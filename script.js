fe0f691abd46a9a9afab31e78c86710431d2b4a1
Cod1ngcase <codingcase95@gmail.com>
2024-09-18 07:01:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ function calculateDates() {
     const yearsDifference = today.getFullYear() - inputDate.getFullYear();
     
     //Create Variables for 3 & 5 Years
-    const thirdYear = new Date(inputDate);
-    thirdYear.setDate(inputDate.getFullYear() + 3);
+    const thirdYear = new Date(new Date(inputDate).setFullYear(new Date(inputDate).getFullYear() + 3)).toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' }).replace(/\//g, '.');
+    const fifthYear = new Date(new Date(inputDate).setFullYear(new Date(inputDate).getFullYear() + 5)).toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' }).replace(/\//g, '.');
     
     // let fifthYear = inputDate.setDate(inputDate.getDate() + 60);
 
@@ -27,16 +27,16 @@ function calculateDates() {
     let warrantyStatus;
 
     if (totalDifference < 36) {
-        warrantyStatus = "In-Warranty All"; // inputDate + 3 Years
+        warrantyStatus = "In-Warranty All " + "Expires " + thirdYear; 
     } else if (totalDifference >= 36 && totalDifference < 60) {
-        warrantyStatus = "In-Warranty Parts";
+        warrantyStatus = "In-Warranty Parts " + "Expires " + fifthYear;
     } else {
-        warrantyStatus = "Out of Warranty";
+        warrantyStatus = "Out of Warranty " + "Expired on " + fifthYear;
     }
-
+    console.log(warrantyStatus)
     // Display the warranty status
     document.getElementById('results').innerHTML = `
-        <div class="results-item"><strong>${warrantyStatus} Expires ${thirdYear}</div>
+        <div class="results-item"><strong>${warrantyStatus}</div>
     `;
 
     const element = document.getElementById("results");
